0adbd7f49283a20265225311e35453b73300e5b4
LemuriiL <nice.duble@mail.ru>
2022-03-28 12:15:15 +0300

    New branch

---
+++ sources/Lab1.18.cpp
@@ -0,0 +1,30 @@
+//
+// Created by LemuriiL on 26.03.2022.
+//
+
+#include <iostream>
+#include <windows.h>
+
+int main() {
+  system("chcp 65001");
+  SetConsoleOutputCP(CP_UTF8);
+  using std::cout;
+  using std::cin;
+  using std::endl;
+  cout << "########УПРАЖНЕНИЕ ВОСЭМНАДЦАТЬ ########\n";
+  int value;
+  int one;
+  int two;
+  int two1;
+  int two2;
+  int result;
+  cout << "введите число => ";
+  cin >> value;
+  one = (value - (value % 10))/100;
+  two = value - (one * 100);
+  two1 = (two - (two % 10))/10;
+  two2 = two % 10;
+  two = (two2 * 10) + two1;
+  result = (two - one) + 1;
+  cout << result;
+}
